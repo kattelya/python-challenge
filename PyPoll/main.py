2c22a142cafc5fbd9fb4c39b4c9b4c1338368621
Kaanggas <44044480+kattelya@users.noreply.github.com>
2019-12-04 16:07:15 -0800

    finish with all my counting code - need to work so it will write to a txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,13 +22,46 @@ with open(file_to_load) as election_data:
         counties.append(row[1])
         candidates.append(row[2])
     
-        total_voters = len(voterID)
-    #using set to know how many counties and candidates actually listed from all the big data!
+    total_voters = len(voterID)
+    #using set to know how many counties and candidates actually are in our data. -geeksforgeeks.org (list and set)
     distinct_counties = list(set(counties))
     distinct_candidate = list(set(candidates))
-print(total_voters)
-#
+
+    #use .count method to find each candidate total vote and convert -- "stackoverflow.com/questions/14540143/python-3-float-decimal-points-precision" = learn from this website to format the percent result"
+    khan_total = candidates.count("Khan")
+    khan = float(khan_total / total_voters) * 100
+    khan_percent = "{:.3f}".format(khan)
+
+    correy_total = candidates.count("Correy")
+    correy = float(correy_total / total_voters) * 100
+    correy_percent = "{:.3f}".format(correy) 
+
+    li_total = candidates.count("Li")
+    li = float(li_total / total_voters) * 100
+    li_percent = "{:.3f}".format(li)
+
+    otooley_total = candidates.count("O'Tooley")
+    otooley = float(otooley_total / total_voters) * 100
+    otooley_percent = "{:.3f}".format(otooley)
+
+    print(khan_total, correy_total, otooley_total, li_total)
+    print(khan_percent, correy_percent, li_percent, otooley_percent)
+    
+    #to find the percentage - each candidate total votes divided by total number votes (total_voters)
+
 print(distinct_counties)
 print(distinct_candidate)
-
-
+output = (
+    f"\nElection Results\n"
+    f"------------------------\n"
+    f"Total Votes: {total_voters}\n"
+    f"------------------------\n"
+    f"Khan: {khan_percent}% ({khan_total})\n"
+    f"Correy: {correy_percent}% ({correy_total})\n"
+    f"Li: {li_percent}% ({li_total})\n"
+    f"Otooley: {otooley_percent}% ({otooley_total})\n"
+    f"------------------------\n"
+    f"Winner: Khan\n"
+    f"------------------------"
+)
+print(output)
