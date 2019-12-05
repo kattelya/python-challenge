14a262519aeae5d0e11b16b51de2b5653ad34203
Kaanggas <44044480+kattelya@users.noreply.github.com>
2019-12-05 12:18:29 -0800

    updating and cleaning my code having extra line on each candidate percent in to 2 line instead of 3 line

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,12 +6,12 @@ import os
 file_to_load = os.path.join("election_data.csv")
 file_to_output = os.path.join("Election Results.txt")
 
-#create 3 variables to hold re-read files that store as a list 
+#create 3 empty variable - list to hold re-read data
 voterID = []
 counties = []
 candidates = []
 
-#Read the csv and conver it into a list 
+#Read the csv
 with open(file_to_load) as election_data:
     reader = csv.reader(election_data, delimiter =",")
     header = next(reader)
@@ -24,31 +24,29 @@ with open(file_to_load) as election_data:
     #use len function on our list to find total_voters in our data
     total_voters = len(voterID)
 
-#using set to know how many counties and candidates actually are in our data. -geeksforgeeks.org (list and set) below 2 lines code is not necessary for this project, but include as future reference if we want to find destinct value on an even larger data set. 
+#using set to know how many counties and candidates actually are in our data. 
+# -geeksforgeeks.org (list and set) below 2 lines code is not necessary for this project, but include as future reference if we want to find destinct value on an even larger data set. 
 # distinct_counties = list(set(counties))
 # distinct_candidate = list(set(candidates))"""
 
-    #use .count method to find each candidate total vote and convert -- "stackoverflow.com/questions/14540143/python-3-float-decimal-points-precision" = learn from this website to format the percent result" 
-    # #to find the percentage - each candidate total votes divided by total number votes (total_voters)
+    #use .count method to find each candidate total vote 
+    #use geeksforgeeks.org/python-format-function/
+    # originally tried using round function but it doesn't work as it will not display the 0000 after the decimal 
     khan_total = candidates.count("Khan")
-    khan = float(khan_total / total_voters) * 100
-    khan_percent = "{:.3f}".format(khan)
-
+    khan_percent = "{:.3f}".format(float(khan_total / total_voters) * 100)
+ 
     correy_total = candidates.count("Correy")
-    correy = float(correy_total / total_voters) * 100
-    correy_percent = "{:.3f}".format(correy) 
-
+    correy_percent = "{:.3f}".format(float(correy_total / total_voters) * 100)
+    
     li_total = candidates.count("Li")
-    li = float(li_total / total_voters) * 100
-    li_percent = "{:.3f}".format(li)
+    li_percent = "{:.3f}".format(float(li_total / total_voters) * 100)
 
     otooley_total = candidates.count("O'Tooley")
-    otooley = float(otooley_total / total_voters) * 100
-    otooley_percent = "{:.3f}".format(otooley)    
+    otooley_percent = "{:.3f}".format(float(otooley_total / total_voters) * 100)
 
 #use PyParagraph class exercise as a reference to write this convenient codes + write to txt file 
 output = (
-    f"\nElection Results\n"
+    f"Election Results\n"
     f"------------------------\n"
     f"Total Votes: {total_voters}\n"
     f"------------------------\n"
@@ -58,8 +56,7 @@ output = (
     f"Otooley: {otooley_percent}% ({otooley_total})\n"
     f"------------------------\n"
     f"Winner: Khan\n"
-    f"------------------------"
-)
+    f"------------------------\n")
 print(output)
 
 with open(file_to_output, "w") as txt_file:
